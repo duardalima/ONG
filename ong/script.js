bd88cbfbd605a2aae418e42981b54e908819d8a7
Maria Eduarda Lima Gonzaga <121740200+duardalima@users.noreply.github.com>
2024-11-17 13:20:41 -0300

    Add files via upload

--- ong/script.js
+++ ong/script.js
@@ -17,3 +17,36 @@ function proximaImg(){
 
     document.getElementById('radio'+cont).checked = true
 }
+
+new Swiper('.card-wrapper.swiper', {
+    loop: true,
+    spaceBetween: 30,
+  
+    // Paginação
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    // Botões de navegação
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // Configuração de breakpoints para responsividade
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 25, // Um slide visível em telas pequenas
+      },
+      768: {
+        slidesPerView: 2, 
+        spaceBetween: 20,// Dois slides visíveis em tablets
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 15, // Três slides visíveis em desktops
+      },
+    },
+  });
